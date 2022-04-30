c93e4fafa3c1aa99734a1dfa628af5ef813928d9
Alexsandron3 <alexsandro0602099@outlook.com>
2022-04-30 16:49:18 -0300

    Cria rota para empresas

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Routes as Router, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import ListCompanies from '../pages/ListCompanies';
 
 export default class Routes extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class Routes extends Component {
         {/*  */}
         {/*  */}
         <Route path="/" element={<Home />} />
+        <Route path="/empresas" element={<ListCompanies />} />
       </Router>
     );
   }
