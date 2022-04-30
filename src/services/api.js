833099818bdf8d45c6389c455914a585c52ca565
Alexsandron3 <alexsandro0602099@outlook.com>
2022-04-30 16:50:32 -0300

    Corrije nome inválido em ternário para verificar em qual ambiente estamos

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === 'Production'
     ? null
     : 'http://localhost/contato_seguro_backend/src/app';
 const api = axios.create({
