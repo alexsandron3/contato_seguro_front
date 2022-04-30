e0838e0202d3ae71e18b48a81dfe1da62aeacc5e
Alexsandron3 <alexsandro0602099@outlook.com>
2022-04-30 15:35:25 -0300

    Cria base de página inicial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import { Grid } from '@mui/material';
 import React, { Component } from 'react';
+import Content from '../components/Content';
 
 export default class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <Grid
+        container
+        spacing={3}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="60vh"
+      >
+        <Grid item xs={8}>
+          <Content title="Contato seguro">
+            <h1>Home</h1>
+          </Content>
+        </Grid>
+      </Grid>
+    );
   }
 }
