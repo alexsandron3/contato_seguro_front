2713e5c1c85ae97f31f635b95e71bc89e93f37bd
Alexsandron3 <alexsandro0602099@outlook.com>
2022-04-30 22:10:35 -0300

    Melhora estilização base

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import Routes from './routes/Routes';
 import { ToastContainer } from 'react-toastify';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import defaultTheme from './theme/theme';
 
 export default class App extends Component {
   render() {
     return (
-      <>
+      <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
         <Routes />
         <ToastContainer />
-      </>
+      </ThemeProvider>
     );
   }
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import MaterialTable from '@material-table/core';
 import React, { Component } from 'react';
 import localization from '../utils/materialTableLocalization';
+import { Paper } from '@material-ui/core';
 export default class Table extends Component {
   constructor(props) {
     super(props);
@@ -14,6 +15,9 @@ export default class Table extends Component {
 
     return (
       <MaterialTable
+        components={{
+          Container: (props) => <Paper {...props} elevation={0} style={{padding: 10}}/>,
+        }}
         columns={columns}
         data={data}
         title={title}
