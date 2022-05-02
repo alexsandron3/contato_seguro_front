4e4219c7ddf910f0806562990aa9e6464e6834a1
Alexsandron3 <alexsandro0602099@outlook.com>
2022-05-02 15:35:49 -0300

    Adicionar top e sidebar ao layout base

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import { ToastContainer } from 'react-toastify';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import defaultTheme from './theme/theme';
+import Topbar from './components/Topbar';
+import Sidebar from './components/Sidebar';
 
 export default class App extends Component {
   render() {
     return (
       <ThemeProvider theme={defaultTheme}>
+        <Topbar />
+        <Sidebar />
         <CssBaseline />
         <Routes />
         <ToastContainer />
