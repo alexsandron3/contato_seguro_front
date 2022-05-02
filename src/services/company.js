b57c6ee235ae058366593515b5998bc6b1939a18
Alexsandron3 <alexsandro0602099@outlook.com>
2022-05-02 18:43:18 -0300

    Cria funções de cadastro e edição de empresas

--- src/services/company.js
+++ src/services/company.js
@@ -13,3 +13,31 @@ export async function listAllCompanies({ showAlert }) {
     if (showAlert) sendAlert(0, error.response.data.mensagem);
   }
 }
+export async function editCompany({ company, showAlert }) {
+  try {
+    const {
+      data: { mensagem, dados },
+    } = await api.put(`/empresa/id.php/${company.id}`, {
+      ...company,
+    });
+    if (showAlert) sendAlert(1, mensagem);
+    return { userEditedWithSuccess: true, dados };
+  } catch (error) {
+    if (showAlert) sendAlert(0, error.response.data.mensagem);
+    return false;
+  }
+}
+export async function newCompany({ company, showAlert }) {
+  try {
+    const {
+      data: { mensagem },
+    } = await api.post('/usuario/', {
+      ...company,
+    });
+    if (showAlert) sendAlert(1, mensagem);
+    return true;
+  } catch (error) {
+    if (showAlert) sendAlert(0, error.response.data.mensagem);
+    return false;
+  }
+}
