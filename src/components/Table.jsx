31ea97b3d1c659210362561d3c21f2aac83f3260
Alexsandron3 <alexsandro0602099@outlook.com>
2022-05-02 00:12:59 -0300

    Altera componentes nativo de crud para personalizados

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,16 +3,15 @@ import MaterialTable from '@material-table/core';
 import React, { Component } from 'react';
 import localization from '../utils/materialTableLocalization';
 import { Paper } from '@material-ui/core';
+import EditIcon from '@mui/icons-material/Edit';
 export default class Table extends Component {
   constructor(props) {
     super(props);
 
     this.state = {};
   }
-
   render() {
-    const { columns, data, title, addRow, editRow, deleteRow } = this.props;
-
+    const { columns, data, title, openForm, deleteRow } = this.props;
     return (
       <MaterialTable
         components={{
@@ -24,24 +23,18 @@ export default class Table extends Component {
         data={data}
         title={title}
         localization={localization}
-        editable={{
-          onRowAdd: (newRow) =>
-            new Promise((resolve, reject) => {
-              addRow(newRow, resolve, reject);
-            }),
-          onRowUpdate: (newRow) =>
-            new Promise((resolve, reject) => {
-              editRow(newRow, resolve, reject);
-            }),
-          onRowDelete: (newRow) =>
-            new Promise((resolve, reject) => {
-              deleteRow(newRow, resolve, reject);
-            }),
-        }}
         options={{
           addRowPosition: 'first',
           actionsColumnIndex: -1,
+          filtering: true,
         }}
+        actions={[
+          {
+            icon: () => <EditIcon />,
+            tooltip: 'Novo usuário',
+            onClick: (props, rowData) => this.props.openForm(rowData),
+          },
+        ]}
       />
     );
   }
